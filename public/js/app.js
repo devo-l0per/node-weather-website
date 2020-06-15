254e3d423d415fdcb0b096572c7021fb075c4eda
devo-l0per <prarjun1911@gmail.com>
2020-06-15 21:15:15 +0530

    remove console.log unnecessary

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 
 
-console.log('client side js file is loaded')
+// console.log('client side js file is loaded')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
